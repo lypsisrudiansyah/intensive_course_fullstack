09de8caee8e7ffe0ff9cdcf8fc4c44c819ecdfa9
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-10-12 23:36:36 +0700

    update

---
+++ chapter_layout/second_layout.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
+
+import 'package:flutter/material.dart';
+
+class SecondLayout extends StatelessWidget {
+  const SecondLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+      ),
+      body: ListView.builder(
+        itemCount: 3,
+        physics: const ScrollPhysics(),
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.grey[200],
+                backgroundImage: const NetworkImage(
+                  "https://i.ibb.co/QrTHd59/woman.jpg",
+                ),
+              ),
+              title: const Text("Jessica Doe"),
+              subtitle: const Text("Programmer"),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
