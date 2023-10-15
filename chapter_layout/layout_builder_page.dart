45d94ebbb70b20c3e5055e58f9ef668d631d4a17
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-10-15 10:23:42 +0700

    🐛 fix(layout_builder_page.dart): refactor code to use LayoutBuilder and conditionally render different layouts based on screen width ✨ feat(layout_builder_page.dart): add support for tablet and desktop views by using GridView.builder with different configurations ✨ feat(layout_builder_page.dart): add support for mobile view by using ListView.builder with Card and ListTile components

--- chapter_layout/layout_builder_page.dart
+++ chapter_layout/layout_builder_page.dart
@@ -10,18 +10,84 @@ class LayoutBuilderPage extends StatelessWidget {
     double screenWidth = MediaQuery.of(context).size.width;
     print('Screen width: $screenWidth');
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Expanded & MediaQuery for Overflow"),
-        actions: const [],
-      ),
-      body: Container(
-        height: 100,
-        width: 100,
-        decoration: const BoxDecoration(
-          color: Colors.blue,
-        ),
-      ),
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        bool isTablet = MediaQuery.of(context).size.width < 1100 && MediaQuery.of(context).size.width >= 850;
+        bool isDesktop = MediaQuery.of(context).size.width >= 1100;
+        bool isMobile = MediaQuery.of(context).size.width < 850;
+
+        if (isDesktop) {
+          return Scaffold(
+            body: GridView.builder(
+              padding: EdgeInsets.zero,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 1.0,
+                crossAxisCount: 6,
+                mainAxisSpacing: 6,
+                crossAxisSpacing: 6,
+              ),
+              itemCount: 8,
+              shrinkWrap: true,
+              physics: const ScrollPhysics(),
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  color: Colors.blue,
+                  child: Center(
+                    child: Text("Desktop View - $index"),
+                  ),
+                );
+              },
+            ),
+          );
+        }
+
+        if (isTablet) {
+          return Scaffold(
+            body: GridView.builder(
+              padding: EdgeInsets.zero,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 1.0,
+                crossAxisCount: 3,
+                mainAxisSpacing: 6,
+                crossAxisSpacing: 6,
+              ),
+              itemCount: 9,
+              shrinkWrap: true,
+              physics: const ScrollPhysics(),
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text("Tablet View - $index"),
+                  ),
+                );
+              },
+            ),
+          );
+        }
+
+        //mobile
+        return Scaffold(
+          body: ListView.builder(
+            itemCount: 3,
+            physics: const ScrollPhysics(),
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.grey[200],
+                    backgroundImage: const NetworkImage(
+                      "https://i.ibb.co/QrTHd59/woman.jpg",
+                    ),
+                  ),
+                  title: const Text("Jessica Doe"),
+                  subtitle: const Text("Programmer"),
+                ),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
